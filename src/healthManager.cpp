f590c0630796df0b5c389f478da7421a979a8eae
maningreen <flibbers@protonmail.com>
2025-03-01 12:16:40 -0700

    added health manager

---
+++ src/healthManager.cpp
@@ -0,0 +1,40 @@
+#include "healthManager.hpp"
+#include "barManager.hpp"
+
+HealthManager::HealthManager(float maxHealth, BarManager b) : BarManager(b), maxHealth(maxHealth), health(maxHealth) {}
+
+HealthManager::HealthManager(float maxHealth, float health, BarManager b) : BarManager(b), maxHealth(maxHealth), health(health) {}
+
+float HealthManager::getHealth() {
+  return health;
+}
+
+void HealthManager::setHealth(float h) {
+  if(h < 0)
+    health = 0;
+  else if(h > maxHealth)
+    health = maxHealth;
+  else
+    health = h;
+}
+
+void HealthManager::applyDamage(float d) {
+  setHealth(health - d);
+}
+
+bool HealthManager::isDead() {
+  return health <= 0;
+}
+
+float HealthManager::getMaxHealth() {
+  return maxHealth;
+}
+
+void HealthManager::setMaxHealth(float m) {
+  if(m < 0)
+    maxHealth = 0;
+  else
+    maxHealth = m;
+}
+
+HealthManager::~HealthManager() {}
---
+++ src/healthManager.hpp
@@ -0,0 +1,26 @@
+#ifndef healthM_h
+#define healthM_h boobies
+
+#include "barManager.hpp"
+
+class HealthManager : public BarManager {
+  private:
+    float health;
+    float maxHealth;
+  public:
+
+    HealthManager(float maxHealth, BarManager b);
+    HealthManager(float maxHealth, float health, BarManager b);
+    ~HealthManager();
+
+    float getHealth();
+    void setHealth(float h);
+    void applyDamage(float d);
+
+    bool isDead();
+    
+    float getMaxHealth();
+    void setMaxHealth(float m);
+};
+
+#endif
