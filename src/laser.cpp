bcc30ab51ce3b63634fc453f433535ba54b687ca
maningreen <flibbers@protonmail.com>
2025-03-12 00:34:28 -0600

    works with unlimited length

--- src/laser.cpp
+++ src/laser.cpp
@@ -32,7 +32,7 @@ void Laser::Render() {
 
   if(preWrap == endPos)
     DrawLineEx(Position, endPos, width, colour);
-  else if(preWrap != endPos) {
+  else while(abs(preWrap.x - endPos.x) > 10 || abs(preWrap.y - endPos.y) > 10) {
     //get intersection
     bool left = endPos.x < 0;
     bool top = endPos.y < 0;
@@ -46,36 +46,13 @@ void Laser::Render() {
     float percY = originPostY / localOffset.y;
 
     Vector2 vectorToCollision = Vector2Scale(localOffset, 1 - (swapX ? percX : percY));
-    Vector2 collisionPosition = Vector2Add(Position, vectorToCollision);
+    Vector2 collisionPosition = Vector2Add(rayOrigin, vectorToCollision);
 
     DrawLineEx(rayOrigin, collisionPosition, width, GREEN);
     
     localOffset = Vector2Subtract(localOffset, vectorToCollision);
     rayOrigin = {swapX ? -collisionPosition.x : collisionPosition.x, !swapX ? -collisionPosition.y : collisionPosition.y};
     preWrap = Vector2Add(rayOrigin, localOffset);
-
-    if(abs(preWrap.x - endPos.x) > 1 || abs(preWrap.y - endPos.y) > 1) {
-      // bool left = endPos.x < 0;
-      // bool top = endPos.y < 0;
-
-      // bool swapX = preWrap.x - Border::Length > preWrap.y - Border::Length;
-      // bool swapY = preWrap.x - Border::Length < preWrap.y - Border::Length;
-    
-      // float originPostX = endPos.x + (swapX ? (left ? Border::Length : -Border::Length) : 0);
-      // float originPostY = endPos.y + (swapY ? (top ? Border::Length : -Border::Length) : 0);
-
-      // float percX = originPostX / localOffset.x;
-      // float percY = originPostY / localOffset.y;
-
-      // Vector2 vectorToCollision = Vector2Scale(localOffset, 1 - (swapX ? percX : percY));
-      // Vector2 collisionPosition = Vector2Add(rayOrigin, vectorToCollision);
-
-      // DrawLineEx(rayOrigin, collisionPosition, width, BLUE);
-
-      // localOffset = Vector2Subtract(localOffset, vectorToCollision);
-      // rayOrigin = {swapX ? -collisionPosition.x : collisionPosition.x, !swapX ? -collisionPosition.y : collisionPosition.y};
-      // preWrap = Vector2Add(rayOrigin, localOffset);
-    }
   }
   DrawLineEx(rayOrigin, endPos, width, colour);
 }
