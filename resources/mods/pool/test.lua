79a89f7a68f215bffb91aa96e9c82c4eca572c49
maningreen <flibbers@protonmail.com>
2026-01-14 10:45:45 -0700

    removed unneeded mods

--- resources/mods/pool/test.lua
+++
@@ -1,6 +0,0 @@
-return {
-  description = "Double player speed",
-  onInit = function(plr)
-    plr.speed = plr.speed * 2
-  end
-}
--- resources/mods/pool/test2.lua
+++
@@ -1,6 +0,0 @@
-return {
-  description = "removes friction",
-  onInit = function()
-    Global.setFriction(1)
-  end
-}
