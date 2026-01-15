6384fb49ce8b3d864398cd53296fc0a5846fb83d
maningreen <flibbers@protonmail.com>
2026-01-14 22:23:55 -0700

    funky rendering for store

--- src/store.cpp
+++ src/store.cpp
@@ -15,26 +15,46 @@ Store::Store() : Entity("StoreManager") {
       arr.erase(arr.begin() + n);
       return;
     }
-    StoreItem* x = new StoreItem(
+    StoreItem x = StoreItem(
       mod.value(),
       (Vector2){
         2 * StoreItem::length * (float)i - StoreItem::length * 2.5f,
         -StoreItem::length / 2.5f
       }
     );
-    x->addTag(tag);
-    addChild(x);
+    items.push_back(x);
     arr.erase(arr.begin() + n);
   }
 
   StoreItem::purchaseHooks.push_back(
     [this](StoreItem& x){
-      for(Entity* item : Engine::getAllChildrenWithTag(this, tag))
-        ((StoreItem*)item)->setState(StoreItem::Passing);
+      for(StoreItem& item : items)
+        item.setState(StoreItem::Passing);
     }
   );
 }
 
 void Store::process(float delta) {
-  if(children.size() == 0) killDefered();
+  for(int i = 0; i < items.size(); i++) {
+    items[i].process(delta);
+    if(!items[i].getValid())
+      items.erase(items.begin() + i--);
+  }
+}
+
+void Store::render() {
+  float l = StoreItem::length * 3.0f + 30;
+  float h = StoreItem::length * .75f + 30;
+  DrawRectangle(-l + 15, -h + 15, l * 2, h * 2, YELLOW);
+  l -= 30;
+  h -= 30;
+  DrawRectangle(-l + 15, -h + 15, l * 2, h * 2, BLACK);
+
+  for(StoreItem& x : items)
+    x.render();
+}
+
+void Store::postProcessingRender() {
+  for(StoreItem& x : items)
+    x.postProcessingRender();
 }
--- src/store.hpp
+++ src/store.hpp
@@ -2,14 +2,18 @@
 #define STORE_H
 
 #include "mod.hpp"
+#include "storeItem.hpp"
 
 // manages the *during* store, not outside of store
 class Store : public Entity {
 private:
   static std::string tag;
+  std::vector<StoreItem> items;
 public:
   Store();
   void process(float delta) override;
+  void render() override;
+  void postProcessingRender() override;
 };
 
 #endif
