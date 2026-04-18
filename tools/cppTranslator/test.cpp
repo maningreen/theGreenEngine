54b08e3a5557157ae0270b1199a347f89a01d4ad
maningreen <flibbers@protonmail.com>
2026-04-17 21:47:15 -0600

    working on printing...

--- tools/cppTranslator/test.cpp
+++ tools/cppTranslator/test.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
+
 class Test {
   public:
-    virtual void testingFunction() {}
+    virtual void testingFunction(float delta) {}
 
     int testingItem;
     int* testingptr;
@@ -8,10 +10,15 @@ class Test {
 
     Test() : testingItem(3) {}
     virtual ~Test() {}
+    void test() { std::cout << "WOOO\n"; }
 };
 
 class Test2 : public Test {
-    virtual void test3() {}
+    virtual void test3(float delta) {}
     Test2() {}
     ~Test2() {}
 };
+
+float sum(float a, float b) {
+    return a + b;
+}
